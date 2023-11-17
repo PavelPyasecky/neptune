ea998344a43219bb78f9276a916e11b819d0c6fa
Pavel Piasetski <pavel.piasetski@sugarcrm.com>
2023-11-17 10:44:44 +0300

    ZOOM Check Zoom Deauthorization Url

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.conf.urls import include
 
+from apiv1 import views
 
 urlpatterns = [
     path('news/', include('apiv1.news.urls')),
+    path('tmp-zoom-deauth-url/', views.TmpViewForTestingZoomDeauthorizationUrl.as_view(), name='tmp_zoom_deauth_url'),
 ]
---
+++ apiv1/views.py
@@ -0,0 +1,15 @@
+import logging
+
+from rest_framework import views, status
+from rest_framework.response import Response
+
+
+logger = logging.getLogger(__name__)
+
+
+class TmpViewForTestingZoomDeauthorizationUrl(views.APIView):
+    def post(self, request, *args, **kwargs):
+        logger.info(f'The Zoom Deauthorization Url works well. The request from Zoom has been received: {request}')
+        logger.info(f'The Zoom Deauthorization Url request.params: {request.query_params}')
+        logger.info(f'The Zoom Deauthorization Url request.data: {request.data}')
+        return Response({'Success.'}, status=status.HTTP_200_OK)
